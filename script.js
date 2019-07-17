56914a64d553f0d0a84cc54b9c9cce528e0a22d8
Hector Trinidad <hmtv16@gmail.com>
2019-07-16 22:24:39 -0500

    get text from input label

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text,
+    done: false
+  };
+  console.log(item);
+}
+
+addItems.addEventListener('submit', addItem);
