2881aead0d9d9d2c412e4d2da31f215cac1e123f
Hector Trinidad <hmtv16@gmail.com>
2019-07-16 22:50:52 -0500

    shows the elements written in the input

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ function addItem(e) {
     done: false
   };
   items.push(item);
+  populateList(items, itemsList);
   this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+      <li>
+        <label for="">${plate.text}</label>
+      </li>
+    `;
+    })
+    .join('');
 }
 
 addItems.addEventListener('submit', addItem);
